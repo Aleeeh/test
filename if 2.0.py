55d378e23d20d7cf9880282d5b6832fb1cde689f
Alee <alebrigaty@gmail.com>
2018-01-10 22:16:05 +0000

    Ejercicio de práctica del if -ordenado-

---
+++ if 2.0.py
@@ -0,0 +1,37 @@
+#CopyRight Aleee cago em deu
+
+number_to_guess = 2
+user_input = int(input("Adivina el number_to_guess"))
+
+if user_input == number_to_guess:
+    print("You win!!")
+else:
+    print("Nope, te quedan 4 intentos")
+
+        user_input = int(input("Adivina el number_to_guess (te quedan 4 intentos): "))
+
+        if user_input == number_to_guess:
+            print("You win!!")
+        else:
+            print("Nope, te quedan 3 intentos")
+
+            user_input = int(input("Adivina el number_to_guess (te quedan 3 intentos): "))
+
+            if user_input == number_to_guess:
+                print("You win")
+            else:
+                print("Nope, te quedan 2 intentos")
+
+                user_input = int(input("Adivina el number_to_guess (te quedan 2 intentos): "))
+
+                if user_input == number_to_guess:
+                    print("You win")
+                else:
+                    print("Nope te queda 1 intento")
+
+                    user_input = int(input("Adivina el number_to_guess (te quedan 1 intentos): "))
+
+                    if user_input == number_to_guess:
+                        print("You win")
+                    else:
+                           print("Nope you lose")
